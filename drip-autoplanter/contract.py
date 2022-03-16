e9687b04c4c4998911d97f8be64cfd20bcb4447a
andrewhibbert <a.hibbert@elsevier.com>
2022-03-16 20:51:14 +0000

    feat_config_args: Configurable options via argparse and configparser

--- drip-autoplanter/contract.py
+++ drip-autoplanter/contract.py
@@ -14,6 +14,7 @@ load_dotenv()
 rpc_url = config['DEFAULT']['rpc_url']
 web3 = Web3(Web3.HTTPProvider(rpc_url))
 
+
 def connect_to_contract(contract_addr, contract_abi):
     contract = web3.eth.contract(address=contract_addr, abi=contract_abi)
     return contract
@@ -22,17 +23,21 @@ def connect_to_contract(contract_addr, contract_abi):
 def send_txn(txn, private_key_encrypt):
     # get key from environment variable
     fernet_key = os.environ['FERNET_KEY']
-    
+
     # decode encrypted key
-    private_key = Fernet(fernet_key.encode()).decrypt(private_key_encrypt.encode()).decode()
-    
+    private_key = Fernet(
+        fernet_key.encode())\
+        .decrypt(private_key_encrypt.encode()).decode()
+
     txn_signed = web3.eth.account.signTransaction(txn, private_key)
     tx_hash = web3.eth.sendRawTransaction(txn_signed.rawTransaction)
     tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
     return tx_receipt
 
+
 def dev(public_address):
     return public_address
+
 
 def get_tx_options(public_address, gas=500000):
     return {
--- drip-autoplanter/garden.py
+++ drip-autoplanter/garden.py
@@ -4,62 +4,78 @@ import json
 import math
 import contract as c
 import time
-from datetime import datetime
 from datetime import timedelta
 
 
 def calculate_seed_to_lp(seeds):
-    return garden_contract.functions.calculateSeedSell(seeds).call()/1000000000000000000
+    return garden_contract.functions.calculateSeedSell(seeds)\
+        .call()/1000000000000000000
+
 
 def get_time_multiplier():
     return garden_contract.functions.currentTimeMultiplier().call()/1000000000
 
+
 def get_garden_balance():
     return garden_contract.functions.getBalance().call()/1000000000000000000
+
 
 def get_total_seeds():
     return garden_contract.functions.marketSeeds().call()
 
+
 def get_user_seeds(addr):
     return garden_contract.functions.getUserSeeds(addr).call()
+
 
 def get_plants_planted(addr):
     return garden_contract.functions.hatcheryPlants(addr).call()
 
+
 def get_seed_to_lp_ratio(addr):
     total_seeds = get_user_seeds(addr)
     total_lp = calculate_seed_to_lp(total_seeds)
-    return total_lp *.95 # tax of 5% on withdrawls
+    return total_lp * .95  # tax of 5% on withdrawls
+
 
 def calculate_next_plant(plants_needed):
     seeds = get_user_seeds(wallet_address)
     total_plants = get_plants_planted(wallet_address)
-    
+
     # each plant generates 1 seed per second
     seeds_per_second = total_plants
     seed_remainder = seeds % 2592000
     seeds_needed = (plants_needed * 2592000) - seed_remainder
-    
+
     time_remaining = seeds_needed / seeds_per_second
     return int(time_remaining)
 
+
 def plant():
-    txn = garden_contract.functions.plantSeeds(c.dev(wallet_address)).buildTransaction(c.get_tx_options(wallet_address, 500000))
+    txn = garden_contract.functions.plantSeeds(c.dev(wallet_address))\
+        .buildTransaction(c.get_tx_options(wallet_address, 500000))
     return c.send_txn(txn, wallet_private_key)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Drip Autoplanter')
-    parser.add_argument("-p", "--plants-to-compound", help="Number of plants to compound at a time", type=int, dest='plants_to_compound', required=True)
-    parser.add_argument("-w", "--wallet-address", help="Your wallet address", type=str, dest='wallet_address', required=True)
+    parser.add_argument(
+        "-w", "--wallet-address",
+        help="Your wallet address",
+        type=str, dest='wallet_address',
+        required=True
+    )
+    parser.add_argument(
+        "-p", "--plants-to-compound",
+        help="Number of plants to compound. Defaults to plants grown a day",
+        type=int,
+        dest='plants_to_compound',
+    )
     args = parser.parse_args()
-
-    plants_to_compound = args.plants_to_compound
-    wallet_address = args.wallet_address
 
     # load config
     config = configparser.ConfigParser()
     config.read('config.ini')
-
     garden_contract_addr = config['DEFAULT']['garden_contract_address']
 
     # load private key
@@ -72,25 +88,33 @@ if __name__ == '__main__':
     # create contract
     garden_contract = c.connect_to_contract(garden_contract_addr, garden_abi)
 
+    # Set wallet address and plants to compound
+    wallet_address = args.wallet_address
+    if args.plants_to_compound:
+        plants_to_compound = args.plants_to_compound
+    else:
+        plants_to_compound = \
+            int((get_plants_planted(wallet_address) * 86400) / 2592000)
+
     while True:
         seeds = get_user_seeds(wallet_address)
         total_plants = get_plants_planted(wallet_address)
         new_plants = math.floor(seeds / 2592000)
         seed_remainder = seeds % 2592000
 
-        # calculate % of seeds lost 
+        # calculate % of seeds lost
         seed_ratio = (1 - (seed_remainder / 2592000))
-        
+
         # this prevents loss of seeds to under 8000 per planting
         seed_range = True if seed_ratio > .997 else False
-        
+
         if new_plants >= plants_to_compound and seed_range:
             plant()
 
             print(f'Planted! {new_plants} added to your garden. Total number of plants now {total_plants + new_plants}')
             print(f'Seeds lost: {seed_remainder}')
 
-            time.sleep(5) # prevent nonce error from being thrown
+            time.sleep(5)  # prevent nonce error from being thrown
         else:
             # if referral adds more plants than we are compounding
             if plants_to_compound > new_plants:
@@ -101,7 +125,7 @@ if __name__ == '__main__':
             seeds_needed = (plants_needed * 2592000) - seed_remainder
 
             print(f"Planting not ready {seeds} seeds available. Need {seeds_needed} more")
-            
+
             # calculate time remaining in seconds
             time_remaining = calculate_next_plant(plants_needed)
 
@@ -111,5 +135,5 @@ if __name__ == '__main__':
                 if t % 30 == 0:
                     break
 
-                print(f"Next Plant Ready: {str(timedelta(seconds=t)).split(':')[0]} hours {str(timedelta(seconds=t)).split(':')[1]} min {str(timedelta(seconds=t)).split(':')[2]} seconds",end="\r")
+                print(f"Next Plant Ready: {str(timedelta(seconds=t)).split(':')[0]} hours {str(timedelta(seconds=t)).split(':')[1]} min {str(timedelta(seconds=t)).split(':')[2]} seconds", end="\r")
                 time.sleep(.5)
